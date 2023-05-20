eb847afeda6693ab96a6a85814b1d456174c9109
Orestis Ioannou <oorestisime@gmail.com>
2023-05-20 12:08:02 +0300

    SHow image tiles in small devices

--- src/pages/categories/[category]/[product].tsx
+++ src/pages/categories/[category]/[product].tsx
@@ -38,7 +38,7 @@ function Product({
             {/* Image gallery */}
             <Tab.Group as="div" className="flex flex-col-reverse">
               {/* Image selector */}
-              <div className="mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none">
+              <div className="mx-auto mt-6 w-full max-w-2xl block lg:max-w-none">
                 <Tab.List className="grid grid-cols-4 gap-6">
                   {product.images.map((image, index) => (
                     <Tab
