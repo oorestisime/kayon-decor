0e2fbd2615baf8169ac54b7d0a989531f1b32d6b
KostasTheodoro <98785109+KostasTheodoro@users.noreply.github.com>
2023-06-01 23:02:37 +0300

    removed svgs from about us

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -53,42 +53,33 @@ export default function ContactUs() {
           <div className="relative px-6 pb-20 pt-24 sm:pt-32 lg:static lg:px-8 lg:py-48">
             <div className="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
               <div className="absolute inset-y-0 left-0 -z-10 w-full overflow-hidden bg-gray-100 ring-1 ring-gray-900/10 lg:w-1/2">
-                <svg
+                {/* <svg
                   className="absolute inset-0 h-full w-full stroke-gray-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
                   aria-hidden="true"
-                >
-                  <defs>
-                    <pattern
-                      id="83fd4e5a-9d52-42fc-97b6-718e5d7ee527"
-                      width={200}
-                      height={200}
-                      x="100%"
-                      y={-1}
-                      patternUnits="userSpaceOnUse"
-                    >
-                      <path d="M130 200V.5M.5 .5H200" fill="none" />
-                    </pattern>
-                  </defs>
-                  <rect
-                    width="100%"
-                    height="100%"
-                    strokeWidth={0}
-                    fill="white"
-                  />
-                  <svg
+                > */}
+                <defs>
+                  <pattern
+                    id="83fd4e5a-9d52-42fc-97b6-718e5d7ee527"
+                    width={200}
+                    height={200}
                     x="100%"
                     y={-1}
-                    className="overflow-visible fill-gray-50"
+                    patternUnits="userSpaceOnUse"
                   >
-                    <path d="M-470.5 0h201v201h-201Z" strokeWidth={0} />
-                  </svg>
-                  <rect
-                    width="100%"
-                    height="100%"
-                    strokeWidth={0}
-                    fill="url(#83fd4e5a-9d52-42fc-97b6-718e5d7ee527)"
-                  />
+                    <path d="M130 200V.5M.5 .5H200" fill="none" />
+                  </pattern>
+                </defs>
+                <rect width="100%" height="100%" strokeWidth={0} fill="white" />
+                <svg x="100%" y={-1} className="overflow-visible fill-gray-50">
+                  <path d="M-470.5 0h201v201h-201Z" strokeWidth={0} />
                 </svg>
+                <rect
+                  width="100%"
+                  height="100%"
+                  strokeWidth={0}
+                  fill="url(#83fd4e5a-9d52-42fc-97b6-718e5d7ee527)"
+                />
+                {/* </svg> */}
               </div>
               <h2 className="text-3xl font-bold tracking-tight text-gray-900">
                 Get in touch
@@ -244,7 +235,7 @@ export default function ContactUs() {
                 <div className="mt-8 flex justify-end">
                   <button
                     type="submit"
-                    className="rounded-md bg-brown-primary px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-brown-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-brown-primary"
+                    className="rounded-md bg-brown-primary px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-brown-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-brown-primary  focus:outline-none focus:ring-2 focus:ring-brown-primary focus:ring-offset-2 focus:ring-offset-gray-50"
                   >
                     Send message
                   </button>
