df6d3684012a2123376d1cbd2ffbeb13904a5ad6
KostasTheodoro <98785109+KostasTheodoro@users.noreply.github.com>
2023-06-02 05:39:06 +0300

    Changed Filters to Categories to match desktop and removed disclosure in single category filtering

--- src/pages/categories/[category]/index.tsx
+++ src/pages/categories/[category]/index.tsx
@@ -101,7 +101,9 @@ function Category({
             >
               <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-6 shadow-xl">
                 <div className="flex items-center justify-between px-4">
-                  <h2 className="text-lg font-medium text-gray-900">Filters</h2>
+                  <h2 className="text-lg font-medium text-gray-900">
+                    Categories
+                  </h2>
                   <button
                     type="button"
                     className="-mr-2 flex h-10 w-10 items-center justify-center p-2 text-gray-400 hover:text-gray-500"
@@ -114,58 +116,31 @@ function Category({
 
                 {/* Filters */}
                 <form className="mt-4">
-                  <Disclosure
-                    as="div"
-                    key={category.name}
-                    className="border-t border-gray-200 pb-4 pt-4"
-                  >
-                    {({ open }) => (
-                      <fieldset>
-                        <legend className="w-full px-2">
-                          <Disclosure.Button className="flex w-full items-center justify-between p-2 text-gray-400 hover:text-gray-500">
-                            <span className="text-sm font-medium text-gray-900">
-                              {category.name}
-                            </span>
-                            <span className="ml-6 flex h-7 items-center">
-                              <ChevronDownIcon
-                                className={classNames(
-                                  open ? "-rotate-180" : "rotate-0",
-                                  "h-5 w-5 transform"
-                                )}
-                                aria-hidden="true"
-                              />
-                            </span>
-                          </Disclosure.Button>
-                        </legend>
-                        <Disclosure.Panel className="px-4 pb-2 pt-4">
-                          <div className="space-y-6">
-                            {subCategories.map((subCategory, index) => (
-                              <div
-                                key={subCategory}
-                                className="flex items-center"
-                              >
-                                <input
-                                  id={subCategory}
-                                  name={`${subCategory}`}
-                                  //@ts-ignore
-                                  checked={checkedSubCategories[subCategory]}
-                                  onChange={() => handleOnChange(subCategory)}
-                                  type="checkbox"
-                                  className="h-4 w-4 rounded border-gray-300 text-brown-primary focus:ring-brown-primary"
-                                />
-                                <label
-                                  htmlFor={`${subCategory}`}
-                                  className="ml-3 text-sm text-gray-500"
-                                >
-                                  {subCategory}
-                                </label>
-                              </div>
-                            ))}
+                  <fieldset>
+                    <div className="px-4 pb-2 pt-4">
+                      <div className="space-y-6">
+                        {subCategories.map((subCategory, index) => (
+                          <div key={subCategory} className="flex items-center">
+                            <input
+                              id={subCategory}
+                              name={`${subCategory}`}
+                              //@ts-ignore
+                              checked={checkedSubCategories[subCategory]}
+                              onChange={() => handleOnChange(subCategory)}
+                              type="checkbox"
+                              className="h-4 w-4 rounded border-gray-300 text-brown-primary focus:ring-brown-primary"
+                            />
+                            <label
+                              htmlFor={`${subCategory}`}
+                              className="ml-3 text-sm text-gray-500"
+                            >
+                              {subCategory}
+                            </label>
                           </div>
-                        </Disclosure.Panel>
-                      </fieldset>
-                    )}
-                  </Disclosure>
+                        ))}
+                      </div>
+                    </div>
+                  </fieldset>
                 </form>
               </Dialog.Panel>
             </Transition.Child>
@@ -178,13 +153,15 @@ function Category({
         </div>
         <div className="pb-24 lg:grid lg:grid-cols-3 lg:gap-x-8 xl:grid-cols-4">
           <aside>
-            <h2 className="sr-only">Filters</h2>
+            <h2 className="sr-only">Categories</h2>
             <button
               type="button"
               className="inline-flex items-center lg:hidden"
               onClick={() => setMobileFiltersOpen(true)}
             >
-              <span className="text-sm font-medium text-gray-700">Filters</span>
+              <span className="text-sm font-medium text-gray-700">
+                Categories
+              </span>
               <PlusIcon
                 className="ml-1 h-5 w-5 flex-shrink-0 text-gray-400"
                 aria-hidden="true"
