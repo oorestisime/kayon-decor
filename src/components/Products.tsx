558c51106bc5d00c7cdb4323fecddf7985c928c9
Orestis Ioannou <oorestisime@gmail.com>
2023-06-03 17:23:50 +0300

    Switch quality only for baskets

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -29,7 +29,7 @@ export const Products = ({ products }: { products: ProductType[] }) => {
           >
             <div className="aspect-h-4 aspect-w-3 bg-gray-200 sm:aspect-none group-hover:opacity-75 sm:h-96">
               <Image
-                quality={30}
+                quality={product.sub_category === "Baskets" ? 20 : 60}
                 placeholder="blur"
                 priority={false}
                 src={product.images[0]}
