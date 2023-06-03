3200d8e6e67c3ee4ded18e1ee2a2d5d1bcc6989d
KostasTheodoro <98785109+KostasTheodoro@users.noreply.github.com>
2023-06-03 21:34:09 +0300

    added react-toastify and costumized it for addtobag

--- src/components/AddToBag.tsx
+++ src/components/AddToBag.tsx
@@ -3,6 +3,9 @@
 import { ProductType, VariantType } from "@/data/store";
 import { GlobalCartContext } from "@/lib/cart";
 import { useContext } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Slide, Zoom, Flip, Bounce } from "react-toastify";
 
 export const AddToBag = ({
   product,
@@ -12,6 +15,8 @@ export const AddToBag = ({
   variant: VariantType;
 }) => {
   const { addItem, cartHasProduct, removeItem } = useContext(GlobalCartContext);
+  const notifyAdd = () => toast.success("Added to bag");
+  const notifyRemove = () => toast.error("Removed from bag");
 
   if (cartHasProduct(product.slug, variant)) {
     return (
@@ -20,10 +25,26 @@ export const AddToBag = ({
           suppressHydrationWarning
           type="submit"
           className="flex max-w-xs flex-1 items-center justify-center rounded-md border border-transparent bg-brown-primary px-8 py-3 text-base font-medium text-white hover:bg-brown-dark focus:outline-none focus:ring-2 focus:ring-brown-primary focus:ring-offset-2 focus:ring-offset-gray-50 sm:w-full"
-          onClick={() => removeItem(product.slug, variant)}
+          onClick={() => {
+            removeItem(product.slug, variant);
+            notifyRemove();
+          }}
         >
           Remove from bag
         </button>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          transition={Slide}
+          newestOnTop
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
       </div>
     );
   }
@@ -33,16 +54,30 @@ export const AddToBag = ({
         suppressHydrationWarning
         type="submit"
         className="flex max-w-xs flex-1 items-center justify-center rounded-md border border-transparent bg-brown-primary px-8 py-3 text-base font-medium text-white hover:bg-brown-dark focus:outline-none focus:ring-2 focus:ring-brown-primary focus:ring-offset-2 focus:ring-offset-gray-50 sm:w-full"
-        onClick={() =>
+        onClick={() => {
           addItem({
             product: product.slug,
             variant,
             quantity: 1,
-          })
-        }
+          });
+          notifyAdd();
+        }}
       >
         Pre-order now!
       </button>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        transition={Slide}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </div>
   );
 };
