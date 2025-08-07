839168a049f948c93931ca1eb5395c61be38a52c
Artemis Toumazi <artemis.toumazi@gmail.com>
2025-08-07 08:00:38 +0300

    Update footer
    Update the copyright line in order to take automatically the year each year.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -43,7 +43,7 @@ export const Footer = () => {
 
         <div className="mt-8 md:order-1 md:mt-0">
           <p className="text-center text-xs leading-5 text-gray-500">
-            &copy; 2023 Kayon Decor, Inc. All rights reserved.
+            &copy; {new Date().getFullYear()} Kayon Decor, Inc. All rights reserved.
           </p>
         </div>
       </div>
